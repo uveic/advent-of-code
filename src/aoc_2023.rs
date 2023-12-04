2ee31ef8da2bda5a71f8a3ff29a81617d4bbb0e3
Víctor González <victor@victorgonzalez.eu>
2023-12-04 13:16:24 +0100

    2023 day 1, part 1

--- src/aoc_2023.rs
+++ src/aoc_2023.rs
@@ -1,7 +1,31 @@
+use std::fs;
+use std::ops::Add;
+
 pub fn day01() -> () {
-    // let content = fs::read_to_string(String::from("input/2022/day01_elves.txt")).unwrap();
+    let content = fs::read_to_string(String::from("input/2023/day01.txt")).unwrap();
+    let lines: Vec<&str> = content.split("\n").filter(|l| l.len() > 0).collect();
+
+    let mut total: i32 = 0;
+    for line in lines {
+        let mut first: Option<char> = None;
+        let mut last: Option<char> = None;
+
+        for x in line.chars() {
+            if x.is_ascii_digit() {
+                if first == None {
+                    first = Some(x);
+                }
+                last = Some(x);
+            }
+        }
+
+        total += String::from(first.unwrap())
+            .add(&last.unwrap().to_string())
+            .parse::<i32>()
+            .unwrap();
+    }
 
     println!("############ 2023 DAY 1 ############");
-    println!("Part 1, result: {:?}", 0);
+    println!("Part 1, result: {:?}", total); // 53386
     println!("Part 2, result: {:#?}", 0);
 }
